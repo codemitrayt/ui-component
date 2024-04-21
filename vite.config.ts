213745ed7e144b070d1e43f7e259b4162d766fb3
codemitrayt <codemitrayt@gmail.com>
2024-04-21 16:55:39 +0530

    fix: package name

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "./lib/index.ts"),
-      name: "react-beautiful-timeline",
+      name: "@codemitrayt/ui-components",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
